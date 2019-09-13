ea7ec8e13996734da131cec16d3d1b07458fcc8f
parentdavid <55148161+parentdavid@users.noreply.github.com>
2019-09-13 12:10:02 +0200

    Update feedback.js

--- docs/feedback.js
+++ docs/feedback.js
@@ -2,7 +2,7 @@
  * Web application
  */
 //const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/07-feedback';
-//const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/16-feedback';
+const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/16-feedback';
 const feedback = {
   // retrieve the existing feedback entries
   get() {
